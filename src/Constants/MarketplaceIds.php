59b71726b04bf46834b38fccbd892bb6eac060b0
Nogrod <strikeomania@gmail.com>
2024-11-29 13:41:14 +0100

    add marketplace ids

---
+++ src/Constants/MarketplaceIds.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace eBay\Sell\Metadata\Constants;
+
+/**
+ * https://developer.ebay.com/api-docs/sell/fulfillment/types/ba:MarketplaceIdEnum
+ */
+class MarketplaceIds
+{
+    public const AU = 'EBAY_AU';
+    public const AT = 'EBAY_AT';
+    public const BE = 'EBAY_BE';
+    public const CA = 'EBAY_CA';
+    public const CH = 'EBAY_CH';
+    public const CN = 'EBAY_CN';
+    public const CZ = 'EBAY_CZ';
+    public const DE = 'EBAY_DE';
+    public const DK = 'EBAY_DK';
+    public const ES = 'EBAY_ES';
+    public const FI = 'EBAY_FI';
+    public const FR = 'EBAY_FR';
+    public const GB = 'EBAY_GB';
+    public const GR = 'EBAY_GR';
+    public const HK = 'EBAY_HK';
+    public const HU = 'EBAY_HU';
+    public const ID = 'EBAY_ID';
+    public const IE = 'EBAY_IE';
+    public const IL = 'EBAY_IL';
+    public const IN = 'EBAY_IN';
+    public const IT = 'EBAY_IT';
+    public const JP = 'EBAY_JP';
+    public const MY = 'EBAY_MY';
+    public const NL = 'EBAY_NL';
+    public const NO = 'EBAY_NO';
+    public const NZ = 'EBAY_NZ';
+    public const PE = 'EBAY_PE';
+    public const PH = 'EBAY_PH';
+    public const PL = 'EBAY_PL';
+    public const PR = 'EBAY_PR';
+    public const PT = 'EBAY_PT';
+    public const RU = 'EBAY_RU';
+    public const SE = 'EBAY_SE';
+    public const SG = 'EBAY_SG';
+    public const TH = 'EBAY_TH';
+    public const TW = 'EBAY_TW';
+    public const US = 'EBAY_US';
+    public const VN = 'EBAY_VN';
+    public const ZA = 'EBAY_ZA';
+    public const EBAY_MOTORS_US = 'EBAY_MOTORS_US';
+}
